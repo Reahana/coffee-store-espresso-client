a5069c21e7c324b46e93a07b2d9edd55e930de60
Reahana <reahana233@gmail.com>
2024-05-16 20:12:47 +0600

    CRUD

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
     const handleAddCoffee = event => {
@@ -19,25 +20,25 @@ const AddCoffee = () => {
         console.log(newCoffee);
 
         // send data to the server
-        // fetch('http://localhost:5000/coffee', {
-        //     method: 'POST',
-        //     headers: {
-        //         'content-type': 'application/json'
-        //     },
-        //     body: JSON.stringify(newCoffee)
-        // })
-        //     .then(res => res.json())
-        //     .then(data => {
-        //         console.log(data);
-        //         if(data.insertedId){
-        //             Swal.fire({
-        //                 title: 'Success!',
-        //                 text: 'Coffee Added Successfully',
-        //                 icon: 'success',
-        //                 confirmButtonText: 'Cool'
-        //               })
-        //         }
-        //     })
+         fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+         })
+         .then(res => res.json())
+          .then(data => {
+                 console.log(data);
+                if(data.insertedId){
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                     })
+            }
+            })
     }
 
     return (
--- src/main.jsx
+++ src/main.jsx
@@ -8,22 +8,23 @@ import {
 } from "react-router-dom";
 
 import AddCoffee from './components/AddCoffee.jsx';
+import UpdateCoffee from './components/UpdateCoffee.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-    //loader: () => fetch('http://localhost:5000/coffee')
+    loader: () => fetch('http://localhost:5000/coffee')
   },
   {
     path: "addCoffee", 
     element: <AddCoffee></AddCoffee>
   }, 
-  // {
-  //   path: 'updateCoffee/:id', 
-  //   element: <UpdateCoffee></UpdateCoffee>, 
-  //   loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
-  // }
+  {
+    path: 'updateCoffee/:id', 
+    element: <UpdateCoffee></UpdateCoffee>, 
+    loader: ({params}) => fetch(`http://localhost:5000/coffee/${params.id}`)
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
